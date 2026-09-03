87a837b38cb6b6bfde99c4be9fdf34a27ace7cef
dilharih <dilhari9895@gmail.com>
2026-09-03 11:15:54 +0530

    fix: preserve main layout child rendering

---
+++ frontend/src/layouts/MainLayout.jsx
@@ -0,0 +1,17 @@
+import Sidebar from "../components/layout/Sidebar";
+import Navbar from "../components/layout/Navbar";
+
+function MainLayout({ children }) {
+  return (
+    <div className="flex min-h-screen bg-slate-100">
+      <Sidebar />
+
+      <div className="flex flex-1 flex-col">
+        <Navbar />
+        <main className="flex-1 p-8">{children}</main>
+      </div>
+    </div>
+  );
+}
+
+export default MainLayout;
