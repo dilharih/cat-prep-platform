0f313034109f85fe36928c55e6339d832bac3d5d
dilharih <dilhari9895@gmail.com>
2026-09-03 12:08:06 +0530

    Apply shared navigation to all application pages

--- frontend/src/routes/AppRouter.jsx
+++ frontend/src/routes/AppRouter.jsx
@@ -10,77 +10,80 @@ import MockTestResultPage from "../features/test/pages/MockTestResultPage";
 import MockTestReviewPage from "../features/test/pages/MockTestReviewPage";
 import AttemptHistoryPage from "../features/practice/pages/AttemptHistoryPage";
 import ProtectedRoute from "../components/auth/ProtectedRoute";
+import SiteLayout from "../layouts/SiteLayout";
 
 function AppRouter() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<LoginPage />} />
+      <SiteLayout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<LoginPage />} />
 
-        <Route
-          path="/dashboard"
-          element={
-            <ProtectedRoute>
-              <Dashboard />
-            </ProtectedRoute>
-          }
-        />
+          <Route
+            path="/dashboard"
+            element={
+              <ProtectedRoute>
+                <Dashboard />
+              </ProtectedRoute>
+            }
+          />
 
-        <Route
-          path="/practice"
-          element={
-            <ProtectedRoute>
-              <PracticePage />
-            </ProtectedRoute>
-          }
-        />
+          <Route
+            path="/practice"
+            element={
+              <ProtectedRoute>
+                <PracticePage />
+              </ProtectedRoute>
+            }
+          />
 
-        <Route
-          path="/history"
-          element={
-            <ProtectedRoute>
-              <AttemptHistoryPage />
-            </ProtectedRoute>
-          }
-        />
+          <Route
+            path="/history"
+            element={
+              <ProtectedRoute>
+                <AttemptHistoryPage />
+              </ProtectedRoute>
+            }
+          />
 
-        <Route
-          path="/mock-tests"
-          element={
-            <ProtectedRoute>
-              <MockTestListPage />
-            </ProtectedRoute>
-          }
-        />
+          <Route
+            path="/mock-tests"
+            element={
+              <ProtectedRoute>
+                <MockTestListPage />
+              </ProtectedRoute>
+            }
+          />
 
-        <Route
-          path="/mock-test/:mockTestId"
-          element={
-            <ProtectedRoute>
-              <MockTestPage />
-            </ProtectedRoute>
-          }
-        />
+          <Route
+            path="/mock-test/:mockTestId"
+            element={
+              <ProtectedRoute>
+                <MockTestPage />
+              </ProtectedRoute>
+            }
+          />
 
-        <Route
-          path="/mock-test-result/:attemptId"
-          element={
-            <ProtectedRoute>
-              <MockTestResultPage />
-            </ProtectedRoute>
-          }
-        />
+          <Route
+            path="/mock-test-result/:attemptId"
+            element={
+              <ProtectedRoute>
+                <MockTestResultPage />
+              </ProtectedRoute>
+            }
+          />
 
-        <Route
-          path="/mock-test-review/:attemptId"
-          element={
-            <ProtectedRoute>
-              <MockTestReviewPage />
-            </ProtectedRoute>
-          }
-        />
-      </Routes>
+          <Route
+            path="/mock-test-review/:attemptId"
+            element={
+              <ProtectedRoute>
+                <MockTestReviewPage />
+              </ProtectedRoute>
+            }
+          />
+        </Routes>
+      </SiteLayout>
     </BrowserRouter>
   );
 }
