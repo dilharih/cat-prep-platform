88f1d65b5ef9b1d0884df944ad4a7cad0cd4bc09
Dilhari <dilhari9895@gmail.com>
2026-09-04 00:29:07 +0530

    Add signup and Google auth services

--- frontend/src/features/auth/services/auth.service.js
+++ frontend/src/features/auth/services/auth.service.js
@@ -5,3 +5,15 @@ export async function login(credentials) {
 
   return response.data;
 }
+
+export async function register(userData) {
+  const response = await api.post("/auth/register", userData);
+
+  return response.data;
+}
+
+export async function googleLogin(idToken) {
+  const response = await api.post("/auth/google", { idToken });
+
+  return response.data;
+}
