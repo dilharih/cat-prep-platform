419ef04d5265daa458fcccabf6976e101d6f2dc7
dilharih <dilhari9895@gmail.com>
2026-09-03 12:59:52 +0530

    Enable global theme provider

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,13 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles/global.css";
+import "./styles/theme-toggle.css";
 import { AuthProvider } from "./context/AuthContext";
-
+import { ThemeProvider } from "./context/ThemeContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
+    <ThemeProvider>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </ThemeProvider>
   </React.StrictMode>
 );
