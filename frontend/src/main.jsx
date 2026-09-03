e03c6ee6da0f089707577584dde69aa845d3c45c
dilharih <dilhari9895@gmail.com>
2026-09-03 13:24:03 +0530

    Load CAT Prep brand color theme globally

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles/global.css";
 import "./styles/theme-toggle.css";
+import "./styles/brand-theme.css";
 import { AuthProvider } from "./context/AuthContext";
 import { ThemeProvider } from "./context/ThemeContext";
 
