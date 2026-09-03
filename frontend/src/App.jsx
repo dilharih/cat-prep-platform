1415a8b1aa7c26dd792c532cba18f6cdf6c8819a
dilharih <dilhari9895@gmail.com>
2026-09-03 11:15:24 +0530

    chore: remove unused App imports

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,4 @@
 import AppRouter from "./routes/AppRouter";
-import Dashboard from "./pages/Dashboard";
-import ProtectedRoute from "./components/auth/ProtectedRoute";
 
 function App() {
   return <AppRouter />;
