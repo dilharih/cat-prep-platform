69fc0c7e2e2de0448cb776ac90f83057b0526e06
Dilhari <dilhari9895@gmail.com>
2026-09-04 00:29:30 +0530

    Add signup page

---
+++ frontend/src/features/auth/pages/SignupPage.jsx
@@ -0,0 +1,12 @@
+import AuthLayout from "../../../layouts/AuthLayout";
+import SignupForm from "../components/SignupForm";
+
+function SignupPage() {
+  return (
+    <AuthLayout>
+      <SignupForm />
+    </AuthLayout>
+  );
+}
+
+export default SignupPage;
