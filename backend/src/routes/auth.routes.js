0a418c80a6b891191614bc345b099bedacde598d
Dilhari <dilhari9895@gmail.com>
2026-09-04 00:30:00 +0530

    Add Google authentication route

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -4,10 +4,12 @@ const {
   login,
   register,
 } = require("../controllers/auth.controller");
+const { googleLogin } = require("../controllers/google-auth.controller");
 
 const router = express.Router();
 
 router.post("/login", login);
 router.post("/register", register);
+router.post("/google", googleLogin);
 
 module.exports = router;
