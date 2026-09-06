e4f14d68838829804f6406d43518631622286c17
Dilhari <dilhari9895@gmail.com>
2026-09-06 18:05:10 +0530

    Add admin question API

---
+++ frontend/src/features/admin/api/adminQuestion.api.js
@@ -0,0 +1,20 @@
+import api from "../../../lib/api";
+
+export async function getAdminMockTestQuestions(mockTestId) {
+  const response = await api.get(`/admin/mock-tests/${mockTestId}/questions`);
+  return response.data.data;
+}
+
+export async function createAdminQuestion(mockTestId, data) {
+  const response = await api.post(`/admin/mock-tests/${mockTestId}/questions`, data);
+  return response.data.data;
+}
+
+export async function updateAdminQuestion(questionId, data) {
+  const response = await api.patch(`/admin/questions/${questionId}`, data);
+  return response.data.data;
+}
+
+export async function removeAdminQuestion(mockTestId, questionId) {
+  return api.delete(`/admin/mock-tests/${mockTestId}/questions/${questionId}`);
+}
